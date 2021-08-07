d507ff41538d3fb04989df75338b1c924c91226b
pidusbhusal <pidusbhusal@gmail.com>
2021-08-07 20:56:04 +0545

    fist commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function TwoTale() {
+    var a, b, prod;
+    a = 1;
+    b = 2;
+
+    for (a = 1; a <= 10; a++) {
+        proud = a * b;
+        console.log(proud);
+
+    }
+}
+
+TwoTale();
+TwoTale();
+TwoTale();
+TwoTale();

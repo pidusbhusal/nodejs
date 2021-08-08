6d643784b9c268f70867491d9a558cea21d076ab
pidusbhusal <pidusbhusal@gmail.com>
2021-08-08 08:29:42 +0545

    did some cange

--- logic.js
+++ logic.js
@@ -1,12 +1,2 @@
-    const idname = document.getElementById("error");
-    console.log(idname);
-
-
-    const classname = document.getElementsByClassName("para");
-    console.log(classname);
-
-    const para = document.getElementsByTagName("p");
-    console.log(para);
-
-    const anything = document.querySelector('p')
-    console.log(anything);
+const para = document.getElementsByTagName('h1');
+console.log(para.innerText)

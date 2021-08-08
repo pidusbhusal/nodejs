3e6edaab7d5505b9768a46da2167fdca49c41613
pidusbhusal <pidusbhusal@gmail.com>
2021-08-08 08:05:45 +0545

    learn to add fles

--- logic.js
+++ logic.js
@@ -1,1 +1,12 @@
-console.log("hello this js is linked")
+    const idname = document.getElementById("error");
+    console.log(idname);
+
+
+    const classname = document.getElementsByClassName("para");
+    console.log(classname);
+
+    const para = document.getElementsByTagName("p");
+    console.log(para);
+
+    const anything = document.querySelector('p')
+    console.log(anything);

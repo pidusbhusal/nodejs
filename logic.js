6e758690bb7f5af8b0006ba0bcb01830cf93a203
pidusbhusal <pidusbhusal@gmail.com>
2021-08-08 15:33:22 +0545

    tried changing the position of script

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
-const p = document.getElementsByTagName('p');
-console.log(p[0].innerText);
+    const p = document.getElementsByTagName('p');
+    console.log(p[0].innerText);
 
-const anotherone = document.querySelector('p');
-console.log(anotherone);setInterval
+    const anotherone = document.querySelector('p');
+    console.log(anotherone);
